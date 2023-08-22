de4af68f214a1ce329ceed6ae778a1b321c18ff2
Cyril Lazar <klazar@mail.ru>
2023-08-22 15:25:07 +0700

    fix

--- app/components/ui/gallery/Gallery.tsx
+++ app/components/ui/gallery/Gallery.tsx
@@ -5,6 +5,6 @@ import { IGalleryItem } from './gallery.interface'
 import GalleryItem from './GalleryItem'
 
 const Gallery: FC<{items:IGalleryItem[]}> = ({items}) => {
-	return <div className={styles.gallery}>{items.map(item=><GalleryItem variant='horizontal' key={item.link} item={item}/>)}</div>
+	return <div className={styles.gallery}>{items.map(item=><GalleryItem variant='vertical' key={item.link} item={item}/>)}</div>
 }
 export default Gallery
